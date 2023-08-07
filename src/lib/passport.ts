c169d13ea629e03b7fcbd57fb9e228810e4986b9
Munkhjargal Batdavaa <munkhjargalbatdavaa@Munkhjargals-iMac.local>
2023-08-07 14:51:45 +0800

    realm upd

--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -40,8 +40,8 @@ passport.use(
   new SteamStrategy(
     {
       returnURL: `${process.env.DOMAIN}/api/auth/return`,
-      realm: `${process.env.DOMAIN}`,
-      apiKey: `${process.env.STEAM_API_KEY}/`,
+      realm: `${process.env.DOMAIN}/`,
+      apiKey: `${process.env.STEAM_API_KEY}`,
       // returnURL: `https://lann-test.netlify.app/api/auth/return`,
       // realm: `https://lann-test.netlify.app/`,
       // apiKey: `F3051B7C4B5A0FA2D4AC2C6E19956E78`,
